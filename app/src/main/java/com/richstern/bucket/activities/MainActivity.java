c6a2b3da40ddba56972befdf5cae95b4a2074a9c
aguynamedrich <aguynamedrich@gmail.com>
2017-02-18 23:16:30 -0800

    Flood fill

--- app/src/main/java/com/richstern/bucket/activities/MainActivity.java
+++ app/src/main/java/com/richstern/bucket/activities/MainActivity.java
@@ -1,8 +1,11 @@
 package com.richstern.bucket.activities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.PointF;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.SeekBar;
@@ -10,6 +13,8 @@ import android.widget.SeekBar;
 import com.jakewharton.rxbinding.view.RxView;
 import com.jakewharton.rxbinding.widget.RxSeekBar;
 import com.richstern.bucket.R;
+import com.richstern.bucket.image.Images;
+import com.richstern.bucket.picasso.AbstractTarget;
 import com.richstern.bucket.rx.OnlyNextObserver;
 import com.richstern.bucket.util.Intents;
 import com.richstern.bucket.util.ViewResizeHelper;
@@ -21,6 +26,7 @@ import com.trello.rxlifecycle.components.support.RxAppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import rx.Observable;
+import rx.Subscriber;
 
 public class MainActivity extends RxAppCompatActivity {
 
@@ -28,6 +34,7 @@ public class MainActivity extends RxAppCompatActivity {
 
     @BindView(R.id.photo_frame) View photoFrame;
     @BindView(R.id.empty_photo) View emptyPhoto;
+    @BindView(R.id.image_container) View imageContainer;
     @BindView(R.id.image) ImageView image;
     @BindView(R.id.red) SeekBar red;
     @BindView(R.id.green) SeekBar green;
@@ -36,6 +43,11 @@ public class MainActivity extends RxAppCompatActivity {
     @BindView(R.id.red_level) View redLevel;
     @BindView(R.id.green_level) View greenLevel;
     @BindView(R.id.blue_level) View blueLevel;
+    @BindView(R.id.threshold_text) View thresholdText;
+    @BindView(R.id.threshold) SeekBar threshold;
+
+    private int[][] pixelData;
+    private int selectedColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +60,7 @@ public class MainActivity extends RxAppCompatActivity {
         ViewResizeHelper.resize(redLevel, AspectRatio.SQUARE, ResizeAnchor.WIDTH);
         ViewResizeHelper.resize(greenLevel, AspectRatio.SQUARE, ResizeAnchor.WIDTH);
         ViewResizeHelper.resize(blueLevel, AspectRatio.SQUARE, ResizeAnchor.WIDTH);
+        ViewResizeHelper.resize(thresholdText, AspectRatio.SQUARE, ResizeAnchor.WIDTH);
 
         RxView.clicks(emptyPhoto)
             .compose(bindToLifecycle())
@@ -56,13 +69,44 @@ public class MainActivity extends RxAppCompatActivity {
                 startActivityForResult(intent, REQUEST_CODE_PICK_PHOTO);
             }));
 
+        RxView.touches(image)
+            .map(event -> new PointF(event.getX(), event.getY()))
+            .distinctUntilChanged()
+            .compose(bindToLifecycle())
+            .subscribe(new Subscriber<PointF>() {
+                @Override
+                public void onCompleted() {
+
+                }
+
+                @Override
+                public void onError(Throwable e) {
+                    Log.d("Rich", e.getClass().getSimpleName() + "," + e.getMessage());
+                }
+
+                @Override
+                public void onNext(PointF pointF) {
+                    floodFill(pointF);
+                }
+            });
+
         Observable.combineLatest(
             RxSeekBar.changes(red),
             RxSeekBar.changes(green),
             RxSeekBar.changes(blue),
             this::toRGB)
+            .doOnNext(selectedColor -> this.selectedColor = selectedColor)
             .compose(bindToLifecycle())
             .subscribe(OnlyNextObserver.forAction(color::setBackgroundColor));
+    }
+
+    private void floodFill(PointF point) {
+        int originX = (int) (point.x / image.getWidth() * pixelData.length);
+        int originY = (int) (point.y / image.getHeight() * pixelData[0].length);
+        Images.floodFill(pixelData, originX, originY, selectedColor, threshold.getProgress());
+        int[] pixels = Images.flatten(pixelData);
+        Bitmap bitmap = Bitmap.createBitmap(pixels, pixelData.length, pixelData[0].length, Bitmap.Config.ARGB_8888);
+        image.setImageBitmap(bitmap);
     }
 
     private int toRGB(int red, int green, int blue) {
@@ -86,8 +130,39 @@ public class MainActivity extends RxAppCompatActivity {
     }
 
     private void photoSelected(Uri data) {
-        Picasso.with(this).load(data).fit().centerCrop().into(image);
-        emptyPhoto.setVisibility(View.GONE);
-        image.setVisibility(View.VISIBLE);
+        Picasso.with(this)
+            .load(data)
+            .into(new AbstractTarget() {
+                @Override
+                public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                    emptyPhoto.setVisibility(View.GONE);
+                    imageContainer.setVisibility(View.VISIBLE);
+                    getBitmapData(bitmap);
+                }
+            });
+    }
+
+    private void getBitmapData(Bitmap bitmap) {
+        int top, left, width, height;
+
+        // Let's just make this a square for simplicity
+        if (bitmap.getWidth() < bitmap.getHeight()) {
+            left = 0;
+            width = height = bitmap.getWidth();
+            top = (bitmap.getHeight() - width) / 2;
+        } else {
+            top = 0;
+            width = height = bitmap.getHeight();
+            left = (bitmap.getWidth() - height) / 2;
+        }
+
+        Bitmap squareBitmap = Bitmap.createBitmap(bitmap, left, top, width, height);
+
+        // Get pixel data and fill our 2D structure
+        int[] pixels = new int[width * height];
+        squareBitmap.getPixels(pixels, 0, width, 0, 0, width, height);
+        pixelData = Images.to2dArray(pixels, width, height);
+
+        image.setImageBitmap(squareBitmap);
     }
 }
---
+++ app/src/main/java/com/richstern/bucket/image/Images.java
@@ -0,0 +1,100 @@
+package com.richstern.bucket.image;
+
+import android.graphics.Point;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Images {
+
+    public static int[][] to2dArray(int[] pixels, int width, int height) {
+        int[][] grid = new int[width][height];
+        int x, y;
+        for (int i = 0; i < pixels.length; i++) {
+            x = i % width;
+            y = i / width;
+            grid[x][y] = pixels[i];
+        }
+        return grid;
+    }
+
+    public static int[] flatten(int[][] grid) {
+        int width = grid.length;
+        int[] flat = new int[width * grid[0].length];
+        int x, y;
+        for (int i = 0; i < flat.length; i++) {
+            x = i % width;
+            y = i / width;
+            flat[i] = grid[x][y];
+        }
+        return flat;
+    }
+
+    public static void floodFill(int[][] imageData, int x, int y, int fillColor, int threshold) {
+        int originalColor = imageData[x][y];
+
+        List<Point> origins = new ArrayList<>();
+        List<Point> newOrigins = new ArrayList<>();
+        List<Point> used = new ArrayList<>();
+
+        origins.add(new Point(x, y));
+
+        while (!origins.isEmpty()) {
+            for (Point origin : origins) {
+                int originX = origin.x;
+                int originY = origin.y;
+
+                int minX = Math.max(0, originX - 1);
+                int minY = Math.max(0, originY - 1);
+                int maxX = Math.min(imageData.length - 1, originX + 1);
+                int maxY = Math.min(imageData[0].length - 1, originY + 1);
+
+                for (int i = minX; i <= maxX; i++) {
+                    for (int j = minY; j <= maxY; j++) {
+                        int colorAtPixel = imageData[i][j];
+                        if (withinThreshold(originalColor, colorAtPixel, threshold)) {
+                            // Fill current pixel
+                            imageData[i][j] = fillColor;
+
+                            // Add new origin if not existing origin
+                            if (i != originX && j != originY) {
+                                Point newOrigin = new Point(i, j);
+                                if (!used.contains(newOrigin)) {
+                                    used.add(newOrigin);
+                                    newOrigins.add(newOrigin);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            origins.clear();
+            origins.addAll(newOrigins);
+            newOrigins.clear();
+        }
+    }
+
+    private static boolean withinThreshold(int originalColor, int colorAtPixel, int threshold) {
+        int originalRed = originalColor & 0x00FF0000 >> 16;
+        int originalGreen = originalColor & 0x0000FF00 >> 8;
+        int originalBlue = originalColor & 0x000000FF;
+        int colorAtPixelRed = colorAtPixel & 0x00FF0000 >> 16;
+        int colorAtPixelGreen = colorAtPixel & 0x0000FF00 >> 8;
+        int colorAtPixelBlue = colorAtPixel & 0x000000FF;
+        int diffRed = Math.abs(originalRed - colorAtPixelRed);
+        int diffGreen = Math.abs(originalGreen - colorAtPixelGreen);
+        int diffBlue = Math.abs(originalBlue - colorAtPixelBlue);
+        float avgDiff = (diffRed + diffGreen + diffBlue) / 3.0f;
+        float percentDiff = avgDiff / 255 * 100;
+        return  percentDiff < threshold;
+    }
+
+    public static int[][] deepCopy(int[][] src) {
+        int[][] copy = new int[src.length][];
+        for (int i = 0; i < src.length; i++) {
+            System.arraycopy(src[i], 0, copy[i], 0, src[i].length);
+        }
+        return copy;
+    }
+}
