7712d963264870f83e11fcc3f1e7363198ca8570
aguynamedrich <aguynamedrich@gmail.com>
2017-02-18 16:44:39 -0800

    Color selection UI

--- app/src/main/java/com/richstern/bucket/activities/MainActivity.java
+++ app/src/main/java/com/richstern/bucket/activities/MainActivity.java
@@ -5,8 +5,10 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 
 import com.jakewharton.rxbinding.view.RxView;
+import com.jakewharton.rxbinding.widget.RxSeekBar;
 import com.richstern.bucket.R;
 import com.richstern.bucket.rx.OnlyNextObserver;
 import com.richstern.bucket.util.Intents;
@@ -18,6 +20,7 @@ import com.trello.rxlifecycle.components.support.RxAppCompatActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import rx.Observable;
 
 public class MainActivity extends RxAppCompatActivity {
 
@@ -26,6 +29,13 @@ public class MainActivity extends RxAppCompatActivity {
     @BindView(R.id.photo_frame) View photoFrame;
     @BindView(R.id.empty_photo) View emptyPhoto;
     @BindView(R.id.image) ImageView image;
+    @BindView(R.id.red) SeekBar red;
+    @BindView(R.id.green) SeekBar green;
+    @BindView(R.id.blue) SeekBar blue;
+    @BindView(R.id.color) View color;
+    @BindView(R.id.red_level) View redLevel;
+    @BindView(R.id.green_level) View greenLevel;
+    @BindView(R.id.blue_level) View blueLevel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +45,9 @@ public class MainActivity extends RxAppCompatActivity {
         ButterKnife.bind(this);
 
         ViewResizeHelper.resize(photoFrame, AspectRatio.SQUARE, ResizeAnchor.WIDTH);
+        ViewResizeHelper.resize(redLevel, AspectRatio.SQUARE, ResizeAnchor.WIDTH);
+        ViewResizeHelper.resize(greenLevel, AspectRatio.SQUARE, ResizeAnchor.WIDTH);
+        ViewResizeHelper.resize(blueLevel, AspectRatio.SQUARE, ResizeAnchor.WIDTH);
 
         RxView.clicks(emptyPhoto)
             .compose(bindToLifecycle())
@@ -43,6 +56,20 @@ public class MainActivity extends RxAppCompatActivity {
                 startActivityForResult(intent, REQUEST_CODE_PICK_PHOTO);
             }));
 
+        Observable.combineLatest(
+            RxSeekBar.changes(red),
+            RxSeekBar.changes(green),
+            RxSeekBar.changes(blue),
+            this::toRGB)
+            .compose(bindToLifecycle())
+            .subscribe(OnlyNextObserver.forAction(color::setBackgroundColor));
+    }
+
+    private int toRGB(int red, int green, int blue) {
+        return 0xFF000000 |
+            ((red << 16) & 0x00FF0000) |
+            ((green << 8) & 0x0000FF00) |
+            (blue & 0x000000FF);
     }
 
     @Override
